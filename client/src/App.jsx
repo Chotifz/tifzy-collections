50a6d68d852914ebb17210f3fd9f7430410346fe
Chotifz <chotife@gmail.com>
2024-09-02 17:25:37 +0700

    add auth login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,10 +17,10 @@ import ShopCheckout from "./pages/shop/checkout";
 import ShopAccount from "./pages/shop/account";
 import CheckAuth from "./components/common/check-auth";
 import UnAuthPage from "./pages/unauth-page";
+import { useSelector } from "react-redux";
 
 function App() {
-  const isAuthenticated = false;
-  const user = { role: "admi" };
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
